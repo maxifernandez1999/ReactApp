cb6d1b3654157ac64328db6afdf8b7fb9042f3f1
maxifernandez1999 <fmaximiliano443@gmail.com>
2021-12-07 23:20:35 -0300

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
+class Helloworld extends React.Component{
+  state = {
+    show: true
+  }
+  toggle = () => {
+    // funcion para setear valores de estados
+    this.setState({show: !this.state.show})
+  }
+  render(){
+    if (this.state.show) {
+      return (
+        <div> {/* siempre tiene que estar encerrado en un elemento */}
+          <h1>Hello World Div {this.props.saludar}</h1>
+          <button onClick={this.toggle}>Show</button> {/* evento click */}
+        </div>
+      );
+    }else{
+      return (
+        <div> {/* siempre tiene que estar encerrado en un elemento */}
+          <button onClick={this.toggle}>Show</button>
+        </div>
+      )
+    }
+    
+  } 
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helloworld saludar="hola como estas"></Helloworld>
     </div>
   );
 }
