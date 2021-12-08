6b04995918d7c49b69ba88443eb71cb49dd4d08f
maxifernandez1999 <fmaximiliano443@gmail.com>
2021-12-07 22:52:38 -0300

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
-
-const HelloWorld = () =>{
-  return(<div>Hellow World Div</div>);
+//componente con funcion flecha
+const HelloWorld = (props) => {
+  return(<div>Hellow World Div {props.saludar}</div>);
 }
+//componente de clase
 // class Hello extends React.Component{
 //   render(){
 //     return(
@@ -13,10 +14,11 @@ const HelloWorld = () =>{
 //   }
 // }
 
+//componente con funcion simple
 function App() {
   return (
     <div className="App">
-      <h1>Hellow World! <HelloWorld></HelloWorld></h1>
+      <h1>Hellow World! <HelloWorld saludar="hola como estas"></HelloWorld></h1>
     </div>
   );
 }
